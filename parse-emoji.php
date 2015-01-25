560c6c0f53c4a922c89af7c27337362febe90688
LiJohnson <598420668@163.com>
2015-01-25 16:18:27 +0800

    update

--- parse-emoji.php
+++ parse-emoji.php
@@ -34,6 +34,22 @@ add_action('wp_footer', function(){
 	var PASS_NODE = /IFRAME|NOFRAMES|NOSCRIPT|SCRIPT|STYLE/i;
 	var NODE_TYPE_ELEMENT = 1;
 	var NODE_TYPE_TEXT = 3;
+
+	var parseRules = [{
+		reg:/:[^:]+:/g,
+		parse:(function(){
+			var cache = JSON.parse(localStorage['EMOJI_DATA']||"{}").emoji;
+			return function(emoji){
+				return cache[emoji.replace(/:/g,'')];
+			}
+		})()
+	},{
+		reg:/\[[^\]]+\]/g,
+		parse:function(emoji){
+			return false;
+		}
+	}];
+
 	var getTextNode = function(node){
 		var nodes = [];
 		for( var i = 0  , subNode ; subNode = node.childNodes[i] ; i++ ){
@@ -50,28 +66,31 @@ add_action('wp_footer', function(){
 	var textToNodes = (function(){
 		var div = document.createElement('DIV');
 		return function(text){
-			var nodes = [];
+			var nodes = [] , documentFragment;
+
+			documentFragment = document.createDocumentFragment();
+
 			div.innerHTML = text;
 			for( var i = 0 , n ; n = div.childNodes[i] ; i++ ){
 				nodes.push(n);
 			}
-			return nodes;
+
+			for( var j = 0 , node ; node = nodes[j] ; j++  ){
+				if(node.nodeName == 'IMG'){
+					node.onerror = onerror;
+				}
+
+				documentFragment.appendChild(node);
+			}
+			return documentFragment;
 		}
 	})();
-
-	var parse = function(opt){
-		//opt.url = 'http://6+img.t.sinajs.cn/t4/appstyle/expression/ext/normal/70/88_org.gif';
-		//return "<span class='wp-emoji' style='background-image:url("+opt.url+")' ></span>";
-		return "<img class='wp-emoji' src='"+opt.url+"' alt="+opt.emoji+" draggable=false />";
-	};
 
 	var onerror = function(e){
 		e.target.parentNode.replaceChild(document.createTextNode(e.target.alt),e.target);
 	}
 
-	var ParseEmoji = function(){};
-
-	ParseEmoji.prototype.load = function(callBack){
+	var load = function(callBack){
 		var stor = window.localStorage || {};
 		var data = JSON.parse(stor['EMOJI_DATA']||"{}");
 		if( data.time && new Date() - data.time < 30*24*60*60*1000  ){
@@ -84,42 +103,27 @@ add_action('wp_footer', function(){
 		},'json');
 	}
 
-	ParseEmoji.prototype.parse = function(node , callBack){
-		var textNodes = getTextNode(node);
-		var url , documentFragment , node , parseNodes , parseNode , parseString , name , unicode , i , j ;
-		this.load(function(emojis){
-			for( i = 0 , node ; node = textNodes[i] ; i++ ){
-				parseString = node.nodeValue.replace(/:[^:]+:/g,function(emojiString){
-					emojiString.replace(/:/g,'');
-					name = emojiString.replace(/:/g,'');
-					url = emojis[name];
-					if(!url)return emojiString;
+	var parseEmoji = function(rootNode , opt){
+		var textNodes = getTextNode(rootNode);
+		var url , documentFragment , node , parseNodes , parseNode , parseString ,	 unicode , i , j ;
+		for( i = 0 , node ; node = textNodes[i] ; i++ ){
+			parseString = node.nodeValue.replace(opt.reg,function(emojiString){
+				 url = opt.parse(emojiString) ;
+				 if(!url)return emojiString;
+				 return "<img class='wp-emoji' src='" + url + "' title='" + emojiString + "' alt='" + emojiString + "' draggable=false />";
+			});
 
-					unicode = (url.match(/unicode\/.+\./)||[""])[0].replace(/(^unicode\/)|(\.$)/g,'');
-
-					return parse( { emoji:emojiString , name:name , url:url,unicode:unicode } );
-				});
-
-				if(parseString != node.nodeValue){
-					documentFragment = document.createDocumentFragment();
-					parseNodes = textToNodes(parseString);
-
-					for( var j = 0 , parseNode ; parseNode = parseNodes[j] ; j++  ){
-						if(parseNode.nodeName == 'IMG'){
-							parseNode.onerror = onerror;
-						}
-
-						documentFragment.appendChild(parseNode);
-					}
-					node.parentNode.replaceChild(documentFragment,node);
-				}
+			if(parseString != node.nodeValue){
+				console.log(parseString,node.nodeValue);
+				node.parentNode.replaceChild(textToNodes(parseString),node);
 			}
-		});
+		}
 	}
 
-	var pe = new ParseEmoji();
-	pe.parse(doc.body);
-	win.PE = ParseEmoji;
+	parseRules.forEach(function(opt){
+		parseEmoji(document.querySelector("body"),opt);
+	});
+
 })(this,document,jQuery);
 </script>
 <?php
